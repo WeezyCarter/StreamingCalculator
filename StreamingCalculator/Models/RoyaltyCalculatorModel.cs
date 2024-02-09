986d91ffdcacba300bc172158808381defb14415
Erik Ochoa <dbait1@gmail.com>
2024-02-09 11:03:56 -0700

    Update Repo

--- StreamingCalculator/Models/RoyaltyCalculatorModel.cs
+++ StreamingCalculator/Models/RoyaltyCalculatorModel.cs
@@ -3,8 +3,13 @@
     public class RoyaltyCalculatorModel
     {
         public long NumberOfStreams { get; set; }
+        public decimal DesiredRoyalty { get; set; }
         public decimal RoyaltyPerStream { get; set; } = 0.00437m; // Example rate
+
         public decimal TotalRoyalty => NumberOfStreams * RoyaltyPerStream;
+
+        public long StreamsForDesiredRoyalty => DesiredRoyalty > 0 ? (long)(DesiredRoyalty / RoyaltyPerStream) : 0;
     }
+
 
 }
